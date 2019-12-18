fe3ed1d95c4023d8c3a7eb7c8fcc63b4ad5bd3a1
Thilakshan <th_thilakshan@hotmail.com>
2019-12-17 20:42:04 -0500

    add theme color start styling nav

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -2,14 +2,17 @@ import React from 'react';
 import {
   Link
 } from "react-router-dom";
+import style from './Navbar.module.scss'
+import classNames from 'classnames'
 
 export default function Navbar(props) {
 
+  // className
 
   // props.sections.map()
   return (
     <>
-      <nav>
+      <nav className={style.siteNav}>
         <ul>
           <li>
             <Link to="/projects">Projects</Link>
