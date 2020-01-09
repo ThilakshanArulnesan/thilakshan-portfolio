68136e349600b22476cc71c9ef6746768b194534
Thilakshan <th_thilakshan@hotmail.com>
2020-01-09 13:27:17 -0500

    resize carousel images

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -9,10 +9,11 @@ export default function ProjectCard({ info, altdesc, image, numImages }) {
   // console.log(numImages);
 
   for (let i = 0; i < numImages; i++) {
-    // console.log(numImages);
+    console.log(numImages);
     images[i] = (
-      <div key={i}>
-        <img src={`./project_images/${info.name}${i}.png`} alt={`${info.name} demo`} />
+      <div key={i} >
+        <img className={`${styles.screenshots}`}
+          src={`./project_images/${info.name}${i}.png`} alt={`${info.name} demo`} />
       </div>
     )
   }
@@ -24,6 +25,8 @@ export default function ProjectCard({ info, altdesc, image, numImages }) {
       {image && <img src={image} alt={altdesc}></img>}
       <p className={styles.projectName}>{info.name}</p>
       <Carousel
+        className={styles.carouselContainer}
+        dynamicHeight
         autoPlay
         interval={5000}
         infiniteLoop
@@ -32,10 +35,10 @@ export default function ProjectCard({ info, altdesc, image, numImages }) {
         {images && images}
       </Carousel>
 
-      {info.homepage && (<p>Site: <a href={info.homepage} target="_blank" rel="noopener noreferrer"> {info.homepage}</a></p>)}
-      {info.html_url && (<p>Github: <a href={info.html_url} target="_blank" rel="noopener noreferrer"> {info.html_url}</a></p>)}
+      {info.homepage && (<p><strong>Website:</strong> <a href={info.homepage} target="_blank" rel="noopener noreferrer"> {info.homepage}</a></p>)}
+      {info.html_url && (<p><strong>Github:</strong> <a href={info.html_url} target="_blank" rel="noopener noreferrer"> {info.html_url}</a></p>)}
 
-      <p>{info.description}</p>
+      <p><strong>Description: </strong>{info.description}</p>
       <hr className={styles.fade}></hr>
     </>
   )
