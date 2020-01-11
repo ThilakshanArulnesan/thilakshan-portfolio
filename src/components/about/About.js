3675b19bc1538374102db57907554896c09b413d
Thilakshan <th_thilakshan@hotmail.com>
2020-01-11 13:21:36 -0500

    fix styling on mobile and desktop for about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,7 +13,7 @@ export default function About(props) {
       <article className={styles.about}>
         <h1>  Thilakshan Arulnesan </h1>
         <img src={image} alt="Thilakshan Arulnesan"></img>
-        <ul>
+        <ul className={styles.links}>
           <li><a href={'https://github.com/ThilakshanArulnesan'}> <img className={styles.logo} src={git} alt='Github logo' /> Github </a></li>
           <li><a href={'https://www.linkedin.com/in/thilakshan/'}><img className={styles.logo} src={linkedInImage} alt='LinkedIn logo' />LinkedIn </a> </li>
         </ul>
